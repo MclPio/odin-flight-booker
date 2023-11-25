bdca7e40ae6e52fb3c6f13d70670d316705df696
MclPio <113801014+MclPio@users.noreply.github.com>
2023-11-25 00:49:50 -0500

    Add flight model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_25_043006) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_25_051229) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,4 +20,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_25_043006) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "flights", force: :cascade do |t|
+    t.bigint "departure_airport_id"
+    t.bigint "arrival_airport_id"
+    t.datetime "start_datetime"
+    t.float "flight_duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["arrival_airport_id"], name: "index_flights_on_arrival_airport_id"
+    t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
+  end
+
+  add_foreign_key "flights", "airports", column: "arrival_airport_id"
+  add_foreign_key "flights", "airports", column: "departure_airport_id"
 end
