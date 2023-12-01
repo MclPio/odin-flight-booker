424cd9cd775057f49e05dad1403e38113e95d3f4
MclPio <113801014+MclPio@users.noreply.github.com>
2023-12-01 18:24:58 -0500

    Leave todo for later

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,12 +5,19 @@ class BookingsController < ApplicationController
     params[:booking][:tickets].to_i.times { @booking.passengers.build }
   end
 
+  #Need to make efficient create action
   def create
+    @booking = Booking.new
+    @booking.flight = @flight
+
+    params[:booking][:passengers_attributes].each do |x|
+      puts x
+    end
 
   end
 
   private
-
+#Need to make sure the way the form is submitting the params is efficient for the create action
   def booking_params
     params.require(:booking).permit(:tickets, :flight_id, passengers_attributes: [:name, :email] )
   end
